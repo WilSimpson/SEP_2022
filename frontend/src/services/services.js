bb82f848b4ca37c4c58ab909f2a0dcfc8a37c1b2
lucasspain13 <lucas-spain@uiowa.edu>
2022-02-11 19:07:18 +0000

    Added initial scaffold for axios call to backend for gamecode

---
+++ frontend/src/services/services.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+// does this have to be the port the backend runs on?
+const API_URL = 'http://localhost:3000/api/auth'
+
+class AuthService {
+    login(email, password) {
+        return axios.post(API_URL + 'signin', {
+            email,
+            password
+        })
+        .then(response => {
+            if (response.data.accessToken) {
+                localStorage.setItem('user', JSON.stringify(response.data));
+            }
+            return response.data;
+        });
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));
+    }
+
+    joinGame(gameCode) {
+        return axios.post(API_URL + 'gameCode', {
+            gameCode
+        })
+        .then(response => {
+            if (response.data.accessToken) {
+                //Reroute to the game?
+                localStorage.setItem('gameCode', JSON.stringify(response.data)); 
+            }
+            return response.data;
+        });
+
+    
+    }
+    
+    clearGame() {
+        localStorage.removeItem('gameCode');
+    }
+}
+
+export default new AuthService();
