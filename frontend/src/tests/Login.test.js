ff9e8a7663da639cbeda490e581fe347fe87fa11
Ella Norton <ella-norton@uiowa.edu>
2022-02-20 13:46:26 -0600

    write Login Component tests

--- frontend/src/tests/Login.test.js
+++ frontend/src/tests/Login.test.js
@@ -1,26 +1,105 @@
-// import axios from 'axios'
 import 'jsdom-global/register';     
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Login from '../components/Login';
 import '../setupTests';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import AuthService from '../services/auth.service';
+
+jest.mock('../services/auth.service');
 
 describe("<Login />", () => {
-    let wrapper;
-    it ("should render my Login component", () => {
-        wrapper = shallow(<Login />);
+    let emailField;
+    let passwordField;
+    let submitButton;
+
+
+    it ("should render the Login component", () => {
+        const shallowWrapper = shallow(<Login />);
+        expect (shallowWrapper);
     });
 
+    describe("Email Field", () => {
+        beforeEach(() => {
+            const { getByTestId } = render(<Login />);
+            emailField =  getByTestId('email-input');
+        });
 
-    it ('should have an email input feild', () => {
-        const { getByTestId } = render(<Login />);
-        const emailFeild =  getByTestId('email-input');
+        it ('should exist', () => {
+            expect(emailField).toBeInTheDocument();
+        });
 
-        expect(emailFeild).toBeInTheDocument();
-        expect(emailFeild.value).toBe('');
-        fireEvent.change(emailFeild, { target: { value: "email" } });
-        expect(emailFeild.value).toBe('email');
+        it ('should accept input', () => {
+            fireEvent.change(emailField, { target: { value: "email@email.com" } });
+            expect(emailField.value).toBe("email@email.com");
+        });
+    });
+
+    describe("Password Field", () => {
+        beforeEach(() => {
+            const { getByTestId } = render(<Login />);
+            passwordField = getByTestId('pass-input');
+        });
+
+        it ('should exist', () => {
+            expect(passwordField).toBeInTheDocument();
+        });
+
+        it ('should accept input', () => {
+            fireEvent.change(passwordField, { target: { value: "password123" } });
+            expect(passwordField.value).toBe("password123");
+        });
+    });
+
+    describe("Submit Button", () => {
+        beforeEach(() => {
+            const { getByTestId } = render(<Login />);
+            submitButton = getByTestId('submit-button');
+            passwordField = getByTestId('pass-input');
+            emailField =  getByTestId('email-input');
+        });
+
+        it ('should exist', () => {
+            expect(submitButton).toBeInTheDocument();
+        });
+
+        it ('should be disabled when fields are empty', () => {
+            expect(passwordField.value).toBe("");
+            expect(emailField.value).toBe("");
+            expect(submitButton).toBeDisabled();
+        });
+
+        it ('should be disabled when email is valid and password is invalid', () => {
+            fireEvent.change(emailField, { target: { value: "valid@email.com" } });
+            fireEvent.change(passwordField, { target: { value: "bad" } });
+            expect(submitButton).toBeDisabled();
+        });
+
+        it ('should be disabled when email is invalid and password is valid', () => {
+            fireEvent.change(emailField, { target: { value: "invalid-email" } });
+            fireEvent.change(passwordField, { target: { value: "morethan6" } });
+            expect(submitButton).toBeDisabled();
+        });
+
+        it ('should not be disabled when email is valid and password is valid', () => {
+            fireEvent.change(emailField, { target: { value: "valid@email.com" } });
+            fireEvent.change(passwordField, { target: { value: "morethan6" } });
+            expect(submitButton).not.toBeDisabled();
+        });
+
+        it ('should call AuthService login when clicked', () => {
+            AuthService.login.mockResolvedValue({
+              response: [{status: 200}]
+            });
+
+            fireEvent.change(emailField, { target: { value: "valid@email.com" } });
+            fireEvent.change(passwordField, { target: { value: "morethan6" } });
+            fireEvent.click(submitButton);
+            expect(AuthService.login).toHaveBeenCalled();
+        });
+
     });
 });
