b06f30b7200a62abe54339e3cff83d1d6be0dfd6
lucasspain13 <lucas-spain@uiowa.edu>
2022-02-21 22:16:54 +0000

    changed auth service to game service

--- frontend/src/services/services.js
+++ frontend/src/services/services.js
@@ -3,27 +3,7 @@ import axios from 'axios';
 // does this have to be the port the backend runs on?
 const API_URL = 'http://localhost:3000/api/auth'
 
-class AuthService {
-    login(email, password) {
-        return axios.post(API_URL + 'signin', {
-            email,
-            password
-        })
-        .then(response => {
-            if (response.data.accessToken) {
-                localStorage.setItem('user', JSON.stringify(response.data));
-            }
-            return response.data;
-        });
-    }
-
-    logout() {
-        localStorage.removeItem('user');
-    }
-
-    getCurrentUser() {
-        return JSON.parse(localStorage.getItem('user'));
-    }
+class GameService {
 
     joinGame(gameCode) {
         return axios.post(API_URL + 'gameCode', {
@@ -45,4 +25,4 @@ class AuthService {
     }
 }
 
-export default new AuthService();
+export default new GameService();
