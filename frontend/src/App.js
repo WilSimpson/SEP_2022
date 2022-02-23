886ba560227a781f9b2245eb749def22400f0d1b
Ella Norton <ella-norton@uiowa.edu>
2022-02-23 12:13:35 -0600

    move routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,40 @@ import './App.css';
 import Home from './components/home';
 import Nav from './components/nav';
 import Footer from './components/stickyFooter';
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
+import Login from './components/Login';
+import AdminDash from './components/AdminDash';
+import ProtectedRoute from './ProtectedRoute';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import configureStore from './store/store';
+
+const { persistor, store } = configureStore();
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
+        <Router>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/login' element={<Login />} />
+                <Route 
+                  exact
+                  path='/admin' 
+                  element={
+                  <ProtectedRoute>
+                    <AdminDash />
+                  </ProtectedRoute>
+                  }
+                />
+              </Routes>
+              </PersistGate>
+          </Provider>
+        </Router>
       <Footer />
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
-import Login from './components/Login';
-import Home from './components/home';
-import AdminDash from './components/AdminDash';
-import ProtectedRoute from './ProtectedRoute';
-import { Provider } from 'react-redux';
-import configureStore from './store/store';
-import { PersistGate } from 'redux-persist/integration/react';
 import { createTheme } from '@material-ui/core/styles'
 import { ThemeProvider } from '@material-ui/core';
 
-const { persistor, store } = configureStore();
 
 const theme = createTheme({
   palette: {
@@ -31,27 +22,8 @@ fontFamily: "Roboto"
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <App />
     </ThemeProvider>
-    <Router>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/login' element={<Login />} />
-            <Route 
-              exact
-              path='/admin' 
-              element={
-              <ProtectedRoute>
-                <AdminDash />
-              </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </PersistGate>
-      </Provider>
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
