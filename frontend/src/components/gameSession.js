866b0ffdd6267ac2141adbbabe6ec260c2f685ad
lucasspain13 <lucas-spain@uiowa.edu>
2022-02-23 19:51:04 +0000

    Page layout and scaffold created for game session

--- frontend/src/components/gameSession.js
+++ frontend/src/components/gameSession.js
@@ -4,12 +4,48 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { Button } from '@mui/material';
+import { ButtonGroup } from '@mui/material';
+
+//Example game format as parsed JSON
+// data: [
+//     {
+//       "id": '1',
+//       'text': "This is the question itself",
+//       "password": "psw",
+//       "onlyChance": false,
+//       "options": [
+//           {
+//           "text": "Option 1",
+//           "link": "1a"
+//             },
+//             {
+//                 "text": "Option 2",
+//                 "link": "1b"
+//             }
+//         ],
+//     }
+// ]
 
 export default function GameSession() {
 
     //a null gamecode will not allow page to load
     //Will have state.code, state.game, and state.formValues
     const { state } = useLocation();
+
+    //Initialize to the first element of the array
+    const [currentQuestion, setQuestion] = useState(state ? state.game[0] : null);
+    //const [responses, setResponses] = useState([]);
+
+    const nextQuestion = () => {
+        //Set the selected option for play game user story
+        //Collect response time data for same story
+        //const nextQuestionLink = currentQuestion.options.link
+        //Scan array (state.game) for the object where id === nextQuestionLink
+        //Set it below
+        setQuestion(null)
+    }
 
   return (
       <div className='container'>
@@ -25,9 +61,18 @@ export default function GameSession() {
             mt: 3,
             mb: 3,
           }}
+          justify="center"
+          alignItems="center"
         >
           <Container maxWidth="sm">
               <Typography>Game Code: { state ? state.code : "No Game Code" } </Typography>
+          </Container>
+          <Container maxWidth="sm">
+            <Typography>Question: {currentQuestion ? currentQuestion.text : "Game not found"}</Typography>
+            {/* Question options or chance game goes here */}
+            <ButtonGroup variant="contained"  alignItems="center" justify="center">
+                <Button color='primary' onClick={nextQuestion} inputProps={{ 'data-testid': 'continue'}} data-testid='continue' disabled={false}>Join Game</Button>
+            </ButtonGroup>
           </Container>
         </Box>
     </Container>
--- frontend/src/testing/GameService.test.js
+++ frontend/src/testing/GameService.test.js
@@ -11,86 +11,26 @@ describe('GameService', () => {
              const response = {
                  response: [{
                      status: 200,
-                     data: [{"1": {'text': "You are a software engineer doing stuff with cars. Make Choices.",
-                     'options': [{'text': "Ignore Result", 'link': "1A"},
-                               {'text': "Technician Re-test", 'link': "2A"},
-                               {'text': "Engineer Re-test", 'link': "5A"},
-                               {'text': "Inform Manager", 'link': "4A"},
-                               {'text': "Email CEO", 'link': "3A"}],
-                   'password': "psw",
-                   'only_chance': false},
-             "1A": {'text': "You ignored the result. Real professional.",
-                    'options': [{'text': "Ignore Crash", 'link': "6A"},
-                              {'text': "Inform Manager", 'link': "7A"},
-                              {'text': "Email CEO", 'link': "8A"}
-                        ],
-                    'password': "psw",
-                    'only_chance': false},
-             "2A": {'text': "Fine. He'll do it again...",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false,
-                    'chance_string': "coin"},
-             "3A": {'text': "Bold Move. The CEO is listening.",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false},
-             "4A": {'text': "You told the manager. He said 'shhh'.",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false},
-             "5A": {'text': "Better have an engineer do it.",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false},
-             "6A": {'text': "Really? You're ignoring this again!?",
-                    'options': [{'text': "Email CEO", 'link': "3B"},
-                              {'text': "Keep Quiet", 'link': "5C"},],
-                    'password': "psw",
-                    'only_chance': false},
-             "8A": {'text': "You have told the CEO.",
-                    'options': [{'text': "Write objective report", 'link': "3C"},
-                              {'text': "Write report with spin", 'link': "5B"},],
-                    'password': "psw",
-                    'only_chance': false},
-             "7A": {'text': "You informed the manager.",
-                    'options': [{'text': "Ignore it", 'link': "6A"},
-                              {'text': "Investigate", 'link': "4B"},],
-                    'password': "psw",
-                    'only_chance': false},
-             "3C": {'text': "So, you were honest.",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false},
-             "5B": {'text': "You little liar.",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false},
-             "5C": {'text': "You kept your mouth shut.",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false},
-             "5C": {'text': "You really tattled to the CEO.",
-                    'options': [],
-                    'password': "psw",
-                    'only_chance': false},
-             "6C": {'text': "You kept the part number. If it ain't broke don't fix it, and if it is, still don't",
-                    'options': [{'text': "Keep Quiet", 'link': "5C"},
-                              {'text': "Email CEO", 'link': "3B"}],
-                    'password': "psw",
-                    'only_chance': false},
-             "2B": {'text': "Yasss organized queen, change that part number.",
-                    'options': [{'text': "Keep Quiet", 'link': "5E"},
-                              {'text': "Email CEO", 'link': "8C"}],
-                    'password': "psw",
-                    'only_chance': false},
-             "2B": {'text': "A snitch, really. This late in the game? The CEO is confused.",
-                    'options': [{'text': "Write objective report", 'link': "3C"},
-                              {'text': "Write report with spin", 'link': "5B"}],
-                    'password': "psw",
-                    'only_chance': false}}]
-                 }],
-             }
+                     data: [
+                        {
+                          "id": '1',
+                          'text': "This is the question itself",
+                          "password": "psw",
+                          "onlyChance": false,
+                          "options": [
+                              {
+                              "text": "Option 1",
+                              "link": "1a"
+                                },
+                                {
+                                    "text": "Option 2",
+                                    "link": "1b"
+                                }
+                            ],
+                        }
+                    ]
+                }]
+                }
              axios.post.mockResolvedValue(response);
  
              const result = GameService.joinGame('555555');
