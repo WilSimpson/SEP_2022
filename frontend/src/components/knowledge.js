daf575e002c6b71d9fe635d8a7b99aa7a15060a8
Joe Chapski <joe.chapski@gmail.com>
2022-02-23 21:08:29 +0000

    created knowledge page and added some text

---
+++ frontend/src/components/knowledge.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import ResponsiveAppBar from './nav';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import StickyFooter from './stickyFooter';
+
+export default function Knowledge() {
+  return (
+      <div className='container'>
+      <CssBaseline />
+      <ResponsiveAppBar />
+      <main>
+        {/* Hero unit */}
+    <Container maxWidth='xl'>
+        <Box
+          sx={{
+            pt: 0,
+            pb: 6,
+            borderRadius: 4,
+            mt: 3,
+            mb: 3,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              How to play a game: 
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              1.) Scan a QR code given to you by the game host
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              2.) Enter the asked for info
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              3.) Answer the question
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              4.) Read the Resault
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              5.) Proceed to given location and enter code if nessesssary
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              6.) Repeat until game is over
+            </Typography>
+          </Container>
+        </Box>
+    </Container>
+      </main>
+      <StickyFooter />
+      </div>
+  );
+}
