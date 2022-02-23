3eb076d67a6574e252ca2edaa9a3a4622ceda950
Wil Simpson <simpson.wil255@gmail.com>
2022-02-23 22:58:24 +0000

    Docker compose development environment working

--- backend/ethicsAdventure/urls.py
+++ backend/ethicsAdventure/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify', TokenVerifyView.as_view(), name = 'token_verify'),
-    re_path('^', include(router.urls)),
+    re_path('^api/', include(router.urls)),
     path('admin/', admin.site.urls)
 ]
