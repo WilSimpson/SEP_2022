ca121e78a6a4e4b47a76e221a8796d68420238eb
Ella Norton <ella-norton@uiowa.edu>
2022-02-24 16:53:32 -0600

    move side menu into single component

--- frontend/src/components/AdminDash.js
+++ frontend/src/components/AdminDash.js
@@ -1,46 +1,5 @@
 import * as React from 'react';
-import { 
-    Box, 
-    Card, 
-    CardActionArea, 
-    Grid, 
-    Typography, Paper, Container } from "@material-ui/core";
-import { Divider } from '@material-ui/core';
-import MuiDrawer from '@mui/material/Drawer';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { AdminDashItems, LogoutMenu } from './SideMenus';
-
-const drawerWidth = 240;
-
-  const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-      '& .MuiDrawer-paper': {
-        position: 'relative',
-        whiteSpace: 'nowrap',
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-        boxSizing: 'border-box',
-        ...(!open && {
-          overflowX: 'hidden',
-          transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-          }),
-          width: theme.spacing(7),
-          [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9),
-          },
-        }),
-      },
-    }),
-  );
+import { SideMenu } from './SideMenu';
 
 export default function AdminDash () {
   const [open, setOpen] = React.useState(true);
@@ -50,41 +9,7 @@ export default function AdminDash () {
 
   return (
     <div>
-      <Box sx={{ display: 'flex' }}>
-         <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            <AdminDashItems parentToChild={open} />
-            <Divider sx={{ my: 1 }} />
-            <LogoutMenu parentToChild={open}/>
-          </List>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-        </Box>
-        </Box>        
+         <SideMenu />
     </div>
     
   );
