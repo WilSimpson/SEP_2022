7df291cde3ac8e3294b26fce85fcfc0c7e368595
Ella Norton <ella-norton@uiowa.edu>
2022-03-02 07:42:39 -0600

    add game/question/option models

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,5 +1,9 @@
+from multiprocessing.sharedctypes import Value
+from tkinter import CASCADE
+from turtle import window_height
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class UserManager(BaseUserManager):
     def create_user(self, email, first_name, last_name, password, role='FACULTY'):
@@ -82,4 +86,25 @@ class User(AbstractBaseUser):
         return self.role == UserRole.ADMIN
 
     objects = UserManager()
+    
+class Game(models.Model):
+    title       = models.CharField(max_length=255)
+    creator_id  = models.IntegerField()
+    code        = models.IntegerField(validators=[MinValueValidator(0),
+                                                   MaxValueValidator(999999)], default=0)
+    active      = models.BooleanField()
+    content     = models.JSONField()
+    
+class Question(models.Model):
+    value       = models.TextField()
+    game_id     = models.ForeignKey(Game, on_delete=models.CASCADE)
+    passcode    = models.CharField(max_length=255)
+    chance      = models.BooleanField()
+    chance_game = models.CharField()    #should we have set options for this?
 
+class Option(models.Model):
+    value           = models.TextField()
+    weight          = models.IntegerField()
+    source_question = models.ForeignKey(Question, on_delete=CASCADE)
+    dest_question   = models.ForeignKey(Question, on_delete=CASCADE)
+    
