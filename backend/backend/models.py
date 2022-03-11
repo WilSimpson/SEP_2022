77d60ce84e2504a226fdf2866b85c3b9d24c85b3
Ella Norton <ella-norton@uiowa.edu>
2022-03-10 19:16:53 -0600

    add question field to model

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -138,5 +138,6 @@ class Team(models.Model):
 class GameSessionAnswer(models.Model):
     time = models.DateTimeField(default=datetime.now)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True)
     option_chosen = models.ForeignKey(Option, on_delete=models.CASCADE)
     
