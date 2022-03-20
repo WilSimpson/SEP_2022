dec436be5ed2f975df7d325fab9d1be37d70e9cb
Joe Chapski <joe.chapski@gmail.com>
2022-03-20 23:46:18 +0000

    added if statement to check if chance game or not

--- frontend/src/components/game/gameSession.js
+++ frontend/src/components/game/gameSession.js
@@ -48,61 +48,119 @@ export default function GameSession() {
     setSelectedOption(null);
     setQuestion(question);
   };
-
-  return (
-    <div className='container'>
-      <CssBaseline />
-      <main>
-        {/* Hero unit */}
-        <Container maxWidth='xl'>
-          <Box
-            sx={{
-              pt: 0,
-              pb: 6,
-              borderRadius: 4,
-              mt: 3,
-              mb: 3,
-            }}
-            justify="center"
-            alignItems="center"
-          >
-            <Container maxWidth="sm">
-              <Typography>
-                {currentQuestion ? currentQuestion.value : 'Game not found'}
-              </Typography>
-              <ButtonGroup
-                variant="contained"
-                alignItems="center"
-                justify="center"
-                orientation="vertical"
-                fullWidth={true}
-              >
-                {currentOptions.map((option) => (
-                  <Button
-                    key={option.id}
-                    variant=
-                      {selectedOption == option ? 'contained' : 'outlined'}
-                    sx={{marginTop: 5}}
-                    data-testid={'option'+ String(option.id)}
-                    onClick={() => setSelectedOption(option)}>
-                    {option.value}
-                  </Button>
-                ))}
-                <Button
-                  color='secondary'
-                  sx={{marginTop: 5}}
-                  onClick={nextQuestion}
-                  inputProps={{'data-testid': 'continue'}}
-                  data-testid='continue'
-                  disabled={!selectedOption}
+  if (currentQuestion.chance) {
+    return (
+      <div className='container'>
+        <CssBaseline />
+        <main>
+          {/* Hero unit */}
+          <Container maxWidth='xl'>
+            <Box
+              sx={{
+                pt: 0,
+                pb: 6,
+                borderRadius: 4,
+                mt: 3,
+                mb: 3,
+              }}
+              justify="center"
+              alignItems="center"
+            >
+              <Container maxWidth="sm">
+                <Typography>
+                  {currentQuestion ? currentQuestion.value : 'Game not found'}
+                </Typography>
+                <ButtonGroup
+                  variant="contained"
+                  alignItems="center"
+                  justify="center"
+                  orientation="vertical"
+                  fullWidth={true}
                 >
-                  Continue
-                </Button>
-              </ButtonGroup>
-            </Container>
-          </Box>
-        </Container>
-      </main>
-    </div>
-  );
+                  {currentOptions.map((option) => (
+                    <Button
+                      key={option.id}
+                      variant=
+                        {selectedOption == option ? 'contained' : 'outlined'}
+                      sx={{marginTop: 5}}
+                      data-testid={'option'+ String(option.id)}
+                      onClick={() => setSelectedOption(option)}>
+                      {option.value}
+                    </Button>
+                  ))}
+                  <Button
+                    color='secondary'
+                    sx={{marginTop: 5}}
+                    onClick={nextQuestion}
+                    inputProps={{'data-testid': 'continue'}}
+                    data-testid='continue'
+                    disabled={!selectedOption}
+                  >
+                    Continue
+                  </Button>
+                </ButtonGroup>
+              </Container>
+            </Box>
+          </Container>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div className='container'>
+        <CssBaseline />
+        <main>
+          {/* Hero unit */}
+          <Container maxWidth='xl'>
+            <Box
+              sx={{
+                pt: 0,
+                pb: 6,
+                borderRadius: 4,
+                mt: 3,
+                mb: 3,
+              }}
+              justify="center"
+              alignItems="center"
+            >
+              <Container maxWidth="sm">
+                <Typography>
+                  {currentQuestion ? currentQuestion.value : 'Game not found'}
+                </Typography>
+                <ButtonGroup
+                  variant="contained"
+                  alignItems="center"
+                  justify="center"
+                  orientation="vertical"
+                  fullWidth={true}
+                >
+                  {currentOptions.map((option) => (
+                    <Button
+                      key={option.id}
+                      variant=
+                        {selectedOption == option ? 'contained' : 'outlined'}
+                      sx={{marginTop: 5}}
+                      data-testid={'option'+ String(option.id)}
+                      disabled={true}>
+                      {option.value}
+                    </Button>
+                  ))}
+                  <Button
+                    color='secondary'
+                    sx={{marginTop: 5}}
+                    onClick={nextQuestion}
+                    inputProps={{'data-testid': 'continue'}}
+                    data-testid='continue'
+                    disabled={!selectedOption}
+                  >
+                    Continue
+                  </Button>
+                </ButtonGroup>
+              </Container>
+            </Box>
+          </Container>
+        </main>
+      </div>
+    );
+  }
 }
