cdf4b071b50eb15e15b8ff650b24d17ec9294a16
Wil Simpson <simpson.wil255@gmail.com>
2022-03-21 22:44:38 +0000

    Linter

--- frontend/src/components/game/gameSession.test.js
+++ frontend/src/components/game/gameSession.test.js
@@ -4,7 +4,7 @@ import {shallow} from 'enzyme';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {render, unmountComponentAtNode} from 'react-dom';
 import {act} from 'react-dom/test-utils';
-import { fireEvent, getByTestId } from '@testing-library/react';
+import {fireEvent, getByTestId} from '@testing-library/react';
 import GamePlayService from '../../services/gameplay';
 
 jest.mock('../../services/gameplay');
@@ -12,50 +12,50 @@ jest.mock('../../services/gameplay');
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useLocation: () => ({
-    state : {
+    state: {
       pathname: 'localhost:3000/gameSession',
       code: '123456',
       game: {
-          id: 1,
-          title: "TestGame",
-          creator_id: 1,
-          code: 123456,
-          active: true,
-          questions: [
-            {
-              id: 1,
-              value: "Question 1",
-              passcode: "123456",
-              chance: false,
-              chance_game: "NO_GAME",
-              game: '1'
-            },
-            {
-              id: 2,
-              value: "Question 2",
-              passcode: "123456",
-              chance: false,
-              chance_game: "NO_GAME",
-              game: '1'
-            },
-          ],
-          options: [
-            {
-              id: 1,
-              value: 'Option 1',
-              dest_question: 2,
-              source_question: 1,
-            },
-            {
-              id: 2,
-              value: 'Option 2',
-              dest_question: 2,
-              source_question: 1,
-            },
-          ],
-        },
+        id: 1,
+        title: 'TestGame',
+        creator_id: 1,
+        code: 123456,
+        active: true,
+        questions: [
+          {
+            id: 1,
+            value: 'Question 1',
+            passcode: '123456',
+            chance: false,
+            chance_game: 'NO_GAME',
+            game: '1',
+          },
+          {
+            id: 2,
+            value: 'Question 2',
+            passcode: '123456',
+            chance: false,
+            chance_game: 'NO_GAME',
+            game: '1',
+          },
+        ],
+        options: [
+          {
+            id: 1,
+            value: 'Option 1',
+            dest_question: 2,
+            source_question: 1,
+          },
+          {
+            id: 2,
+            value: 'Option 2',
+            dest_question: 2,
+            source_question: 1,
+          },
+        ],
+      },
       formValue: null,
-    }
+    },
   }),
 }));
 
@@ -129,9 +129,9 @@ describe('<GameSession />', () => {
     expect(GamePlayService.answerQuestion).toHaveBeenCalled();
     await act(() => promise);
   });
-  it ('should change the text on the screen when "Continue" button clicked', () => {
+  it('should change the text on the screen when "Continue" button clicked', () => {
     expect(container.textContent).toContain(
-      'Question 1',
+        'Question 1',
     );
     const promise = Promise.resolve();
     GamePlayService.answerQuestion.mockResolvedValue({
@@ -141,7 +141,7 @@ describe('<GameSession />', () => {
     fireEvent.click(option2);
     fireEvent.click(continueButton);
     expect(container.textContent).toContain(
-      'Question 2',
+        'Question 2',
     );
     expect(continueButton).toBeDisabled();
   });
