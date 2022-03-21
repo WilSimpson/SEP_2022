c857fa4f12098a12f3d704583b636bc40a753819
Wil Simpson <simpson.wil255@gmail.com>
2022-03-21 23:38:08 +0000

    Added download button

--- frontend/src/pages/faculty/generateQR.js
+++ frontend/src/pages/faculty/generateQR.js
@@ -1,43 +1,57 @@
-import {Container, Grid, Paper, TextField} from '@mui/material';
+import {Button, Grid, Paper, TextField} from '@mui/material';
 import React from 'react';
 import QRCode from 'react-qr-code';
 import AuthenticatedLayout from '../../components/layout/authenticated.layout';
+import {saveSvgAsPng} from 'save-svg-as-png';
+import {alertService} from '../../services/alert';
 
 export default function GenerateQRPage() {
   const [url, setURL] = React.useState('');
 
   return (
     <AuthenticatedLayout>
-      <Container maxWidth="lg" sx={{mt: 4, mb: 4}}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={8} lg={9}>
-            <Paper
-              elevation={7}
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            elevation={7}
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 240,
+            }}
+          >
+            <h1>Generate QR Code</h1>
+            <p>Enter the URL for the QR code to link to</p>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="outlined-required"
+                label="QR URL"
+                defaultValue=''
+                onChange={(e) => setURL(e.target.value)}
+              />
+            </Grid>
+            <Button
+              onClick={() => {
+                if (url == '') {
+                  alertService.error('No URL set');
+                } else {
+                  saveSvgAsPng(
+                      document.getElementById('qr-code').firstChild,
+                      'qrcode.png',
+                  );
+                }
               }}
             >
-              <h1>Generate QR Code</h1>
-              <p>Enter the URL for the QR code to link to</p>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  id="outlined-required"
-                  label="QR URL"
-                  defaultValue=''
-                  onChange={(e) => setURL(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                {url == '' ? null : <QRCode value={url} />}
-              </Grid>
-            </Paper>
-          </Grid>
+              Download QR Code
+            </Button>
+            <div id="qr-code">
+              {url == '' ? null : <QRCode value={url} />}
+            </div>
+          </Paper>
         </Grid>
-      </Container>
+      </Grid>
     </AuthenticatedLayout>
   );
 }
