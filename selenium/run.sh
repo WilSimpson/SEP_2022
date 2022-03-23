2056d62913c36f3613f3a40fe6ffb3ed426bd684
Wil Simpson <simpson.wil255@gmail.com>
2022-03-23 18:18:40 +0000

    Started selenium testing

---
+++ selenium/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PWD=$(pwd)
+CHROME_DRIVER_DIR=browsers/linux64
+CHROME_DRIVER_FILE=$CHROME_DRIVER_DIR/chromedriver
+
+if [ command -v chromedriver > /dev/null 2>&1 ]; then
+  echo 'chromedriver already found'
+else
+  echo 'chromedriver not found in PATH'
+  if [ ! -f $CHROME_DRIVER_FILE ]; then
+    echo 'local chromedriver not found'
+    echo 'Downloading chromedriver'
+    mkdir -p $CHROME_DRIVER_DIR
+    LATEST_VERSION=$(curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+    wget http://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_linux64.zip
+    unzip chromedriver_linux64 -d browsers/linux64
+    rm chromedriver_linux64.zip
+  else
+    echo 'local chromedriver found'
+  fi
+fi
+
+node src/sample-test.js
