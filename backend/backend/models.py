bb6c58bfbcabaa4b51a02d6a716a94d5d88bd767
Joe Chapski <joe.chapski@gmail.com>
2022-03-25 15:10:57 +0000

    added course model

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -163,3 +163,8 @@ class GameSessionAnswer(models.Model):
     
     created_at  = AutoDateTimeField(default=timezone.now)
     updated_at  = AutoDateTimeField(default=timezone.now)
+class Course(models.Model):        
+    name       = models.TextField()
+    section        = models.CharField(max_length=32)
+    department    = models.CharField(max_length=32)
+    number      = models.IntegerField
