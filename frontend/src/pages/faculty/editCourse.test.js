a151b1e566323d7dee3dbd2c3a40b04c93264f58
lucasspain13 <lucas-spain@uiowa.edu>
2022-03-27 17:59:20 +0000

    Tests in place

---
+++ frontend/src/pages/faculty/editCourse.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import EditCourse from './editCourse';
+import '../../setupTests';
+import '@testing-library/jest-dom/extend-expect';
+import {render, fireEvent} from '@testing-library/react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+describe('<EditCourse />', () => {
+  it('should render the add course page', () => {
+    expect(
+        shallow(
+            <BrowserRouter>
+              <Routes>
+                <Route path="*" element={<EditCourse />} />
+              </Routes>
+            </BrowserRouter>,
+        ),
+    );
+  });
+
+  it('should route towards the faculty dashboard', () => {
+    render(
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<EditCourse />} />
+          </Routes>
+        </BrowserRouter>,
+    );
+    const back = document.querySelector('[data-testid=back]');
+
+    expect(back).toBeInTheDocument();
+    fireEvent.click(back);
+    expect(back).toBeInTheDocument()
+  });
+});
