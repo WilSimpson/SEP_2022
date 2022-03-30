3e716416f9ec9780a8b7098f786a0bff5862a046
Joe Chapski <joe.chapski@gmail.com>
2022-03-30 18:18:23 +0000

    added the password reset to the paths

--- backend/ethicsAdventure/urls.py
+++ backend/ethicsAdventure/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     path('api/games/create/', GameViewSet.as_view({'post':'create'}), name='create_game'),
     path('api/gameSession/answer/', GameSessionAnswerViewSet.as_view({'post':'create'}), name='record_answer'),
     path('api/teams/createTeam/', views.create_team, name='createTeam'),
+    path(r'^api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     re_path('^api/', include(router.urls)),
     path('admin/', admin.site.urls)
 ]
