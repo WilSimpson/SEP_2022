fed5962c350c95ecc9fe53e9cbd563b8d779cc28
Joe Chapski <joe.chapski@gmail.com>
2022-03-30 18:19:15 +0000

    added password reset to the installed apps

--- backend/ethicsAdventure/settings.py
+++ backend/ethicsAdventure/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'django_rest_passwordreset',
     'backend',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders'
