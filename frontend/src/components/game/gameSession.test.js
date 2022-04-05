fcde45e4727871dbb9749cd84c10094fac4072e1
Ella Norton <ella-norton@uiowa.edu>
2022-04-04 21:07:11 -0500

    all tests passing again...

--- frontend/src/components/game/gameSession.test.js
+++ frontend/src/components/game/gameSession.test.js
@@ -134,69 +134,73 @@ describe('<GameSession />', () => {
     
   });
   describe('Game Play', () => {
-    // it('should render', () => {
-    //   shallow(
-    //       <BrowserRouter>
-    //         <Routes>
-    //           <Route path="*" element={<GameSession />} />
-    //         </Routes>
-    //       </BrowserRouter>,
-    //   );
-    // });
-    // it('should have correct text content', () => {
-    //   expect(container.textContent).toContain(
-    //       'Question 1',
-    //   );
-    // });
-    // it('should have disabled "Continue" button when option not selected', () => {
-    //   expect(continueButton).toBeDisabled();
-    // });
-    // it('should not have disabled "Continue" button after option1 selected', () => {
-    //   fireEvent.click(option1);
-    //   expect(continueButton).not.toBeDisabled();
-    // });
-    // it('should not have disabled "Continue" button after option2 selected', () => {
-    //   fireEvent.click(option2);
-    //   expect(continueButton).not.toBeDisabled();
-    // });
-    // it('should call answerQuestion when "Continue" button clicked', async () => {
-    //   const promise = Promise.resolve();
-    //   GamePlayService.answerQuestion.mockResolvedValue({
-    //     response: jest.fn(() => promise),
-    //   });
+    it('should render', () => {
+      shallow(
+          <BrowserRouter>
+            <Routes>
+              <Route path="*" element={<GameSession />} />
+            </Routes>
+          </BrowserRouter>,
+      );
+    });
+    it('should have correct text content', () => {
+      expect(container.textContent).toContain(
+          'Question 1',
+      );
+    });
+    it('should have disabled "Continue" button when option not selected', () => {
+      expect(continueButton).toBeDisabled();
+    });
+    it('should not have disabled "Continue" button after option1 selected', () => {
+      fireEvent.click(option1);
+      expect(continueButton).not.toBeDisabled();
+    });
+    it('should not have disabled "Continue" button after option2 selected', () => {
+      fireEvent.click(option2);
+      expect(continueButton).not.toBeDisabled();
+    });
+    it('should call answerQuestion when "Continue" button clicked', async () => {
+      const promise = Promise.resolve();
+      GamePlayService.answerQuestion.mockResolvedValue({
+        response: jest.fn(() => promise),
+      });
   
-    //   fireEvent.click(option1);
-    //   fireEvent.click(continueButton);
-    //   expect(GamePlayService.answerQuestion).toHaveBeenCalled();
-    //   await act(() => promise);
-    // });
-    // it('should change the text on the screen when "Continue" button clicked', () => {
-    //   expect(container.textContent).toContain(
-    //       'Question 1',
-    //   );
-    //   const promise = Promise.resolve();
-    //   GamePlayService.answerQuestion.mockResolvedValue({
-    //     response: jest.fn(() => promise),
-    //   });
+      fireEvent.click(option1);
+      fireEvent.click(continueButton);
+      expect(GamePlayService.answerQuestion).toHaveBeenCalled();
+      await act(() => promise);
+    });
+    it('should change the text on the screen when "Continue" button clicked', async () => {
+      expect(container.textContent).toContain(
+          'Question 1',
+      );
+      const promise = Promise.resolve();
+      GamePlayService.answerQuestion.mockResolvedValue({
+        response: jest.fn(() => promise),
+      });
   
-    //   fireEvent.click(option1);
-    //   fireEvent.click(continueButton);
-    //   expect(container.textContent).toContain(
-    //       'Question 2',
-    //   );
-    // });
-    // it('should call updateCurrentQuestion when "Continue" button clicked', () => {
-    //   const promise = Promise.resolve();
-    //   GamePlayService.answerQuestion.mockResolvedValue({
-    //     response: jest.fn(() => promise),
-    //   });
-    //   GamePlayService.updateCurrentQuestion.mockResolvedValue({
-    //     response: jest.fn(),
-    //   });
-    //   fireEvent.click(option1);
-    //   fireEvent.click(continueButton);
-    //   expect(GamePlayService.updateCurrentQuestion).toHaveBeenCalled();
-    // });
+      fireEvent.click(option1);
+      fireEvent.click(continueButton);
+      await waitFor(() => {
+        expect(container.textContent).toContain(
+          'Question 2',
+        );
+      });
+    });
+    it('should call updateCurrentQuestion when "Continue" button clicked', async () => {
+      const promise = Promise.resolve();
+      GamePlayService.answerQuestion.mockResolvedValue({
+        response: jest.fn(() => promise),
+      });
+      GamePlayService.updateCurrentQuestion.mockResolvedValue({
+        response: jest.fn(),
+      });
+      fireEvent.click(option1);
+      fireEvent.click(continueButton);
+      await waitFor(() => {
+        expect(GamePlayService.updateCurrentQuestion).toHaveBeenCalled();
+      });
+    });
   });
   
   describe('Complete Game', () => {
@@ -204,87 +208,75 @@ describe('<GameSession />', () => {
     beforeEach(async () => {
       // to get to the last page
       const promise = Promise.resolve();
-      act(() => {
-        GamePlayService.answerQuestion.mockResolvedValue({
-          response: jest.fn(() => promise),
-        });
+      GamePlayService.answerQuestion.mockResolvedValue({
+        response: jest.fn(() => promise),
       });
       fireEvent.click(option2);
       fireEvent.click(continueButton);
       completeButton = await findByTestId(container, 'complete');
-      // completeButton = getByTestId(container, 'complete');
-      // await act(() => promise);
     });
-    it ('"Complete" button should exist when on final question page', async () => {
-      
+    it ('"Complete" button should exist when on final question page', () => {
       expect(container.textContent).toContain(
         'Complete',
       );
     });
-    // it('"Complete" button should not be disabled', () => {
-    //   expect(completeButton).not.toBeDisabled();
-    // });
-    // it('should call teamCompleteGame when "Complete" button clicked', async () => {
-    //   const promise = Promise.resolve();
-    //   GamePlayService.teamCompleteGame.mockResolvedValue({
-    //     response: jest.fn(() => promise),
-    //   });
+    it('"Complete" button should not be disabled', () => {
+      expect(completeButton).not.toBeDisabled();
+    });
+    it('should call teamCompleteGame when "Complete" button clicked', async () => {
+      const promise = Promise.resolve();
+      GamePlayService.teamCompleteGame.mockResolvedValue({
+        response: jest.fn(() => promise),
+      });
   
-    //   fireEvent.click(completeButton);
-    //   expect(GamePlayService.teamCompleteGame).toHaveBeenCalledWith(TEAM_ID);
-    //   await act(() => promise);
-    // });
+      fireEvent.click(completeButton);
+      expect(GamePlayService.teamCompleteGame).toHaveBeenCalledWith(TEAM_ID);
+      await act(() => promise);
+    });
   });
 });
-// describe('<GameSession />', () => {
-//   let continueButton;
-//   let option1;
-//   let option2;
-//   let option3;
-//   let option4;
-//   let chanceButton;
-//   beforeEach(() => {
-//     act(() => {
-//       render(
-//           <BrowserRouter>
-//             <Routes>
-//               <Route path="*" element={<GameSession />} />
-//             </Routes>
-//           </BrowserRouter>,
-//           container,
-//       );
-//     });
-//     const promise = Promise.resolve();
-//     GamePlayService.answerQuestion.mockResolvedValue({
-//       response: jest.fn(() => promise),
-//     });
-//     continueButton = getByTestId(container, 'continue');
-//     option1 = getByTestId(container, 'option1');
-//     option2 = getByTestId(container, 'option2');
-//     fireEvent.click(option1);
-//     fireEvent.click(continueButton);
-//     chanceButton = getByTestId(container, 'chance')
-//     option3 = getByTestId(container, 'option3');
-//     option4 = getByTestId(container, 'option4');
-//   });
-//   it('should have disabled "Continue" button when option not selected', () => {
-//     expect(continueButton).toBeDisabled();
-//   });
-//   it('option3 should be disabled', () => {
-//     expect(option3).toBeDisabled();
-//   });
-//   it('option4 should be disabled', () => {
-//     expect(option4).toBeDisabled();
-//   });
-//   it('should call random when chance is clicked', () => {
-//     fireEvent.click(chanceButton);
-//     expect(GamePlayService.random).toHaveBeenCalled();
-//   });
-// });
-
-
-function tick() {
-  return new Promise(resolve => {
-    setTimeout(resolve, 0);
-  })
-}
+describe('<GameSession />', () => {
+  let continueButton;
+  let option1;
+  let option2;
+  let option3;
+  let option4;
+  let chanceButton;
+  beforeEach(async () => {
+    act(() => {
+      render(
+          <BrowserRouter>
+            <Routes>
+              <Route path="*" element={<GameSession />} />
+            </Routes>
+          </BrowserRouter>,
+          container,
+      );
+    });
+    const promise = Promise.resolve();
+    GamePlayService.answerQuestion.mockResolvedValue({
+      response: jest.fn(() => promise),
+    });
+    continueButton = getByTestId(container, 'continue');
+    option1 = getByTestId(container, 'option1');
+    option2 = getByTestId(container, 'option2');
+    fireEvent.click(option1);
+    fireEvent.click(continueButton);
+    chanceButton = await findByTestId(container, 'chance')
+    option3 = getByTestId(container, 'option3');
+    option4 = getByTestId(container, 'option4');
+  });
+  it('should have disabled "Continue" button when option not selected', () => {
+    expect(continueButton).toBeDisabled();
+  });
+  it('option3 should be disabled', () => {
+    expect(option3).toBeDisabled();
+  });
+  it('option4 should be disabled', () => {
+    expect(option4).toBeDisabled();
+  });
+  it('should call random when chance is clicked', () => {
+    fireEvent.click(chanceButton);
+    expect(GamePlayService.random).toHaveBeenCalled();
+  });
+});
