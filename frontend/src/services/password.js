a6223b41e8d4709727734a93875547ca28464d13
Joe Chapski <joe.chapski@gmail.com>
2022-04-08 03:11:42 +0000

    added a service to change the passowrd

--- frontend/src/services/password.js
+++ frontend/src/services/password.js
@@ -5,5 +5,8 @@ class PasswordService {
   checkEmail(user) {
     return axios.post(API_URL + '/password_reset/', {email: user});
   }
+  changePassword(t, pw) {
+    return axios.post(API_URL + '/password_reset/confirm/', {'password': pw.toString(), 'token': t.toString()});
+  }
 }
 export default new PasswordService();
