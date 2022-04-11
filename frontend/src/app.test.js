47d25d42f1bb8bd4e64509907f869e506ec16611
Ella Norton <ella-norton@uiowa.edu>
2022-04-11 17:56:19 -0500

    app.js test fix

--- frontend/src/app.test.js
+++ frontend/src/app.test.js
@@ -1,3 +1,17 @@
-test('filler test', () => {
-  expect(true).toBe(true);
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect, jest} from '@jest/globals';
+import {act} from 'react-dom/test-utils';
+import {render, unmountComponentAtNode} from 'react-dom';
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
+import App from './app';
+
+describe('<App />', () => {
+  describe('child components it renders', () => {
+    it ('should render <Router/>', () => {
+      const wrapper = shallow(<App />);
+      console.log(wrapper.debug());
+      expect(wrapper.find(BrowserRouter)).toHaveLength(1);
+    });
+  });
 });
