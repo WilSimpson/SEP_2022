9f400a6011bf801e19cc501dcc455c9af75c5377
Wil Simpson <simpson.wil255@gmail.com>
2022-04-15 20:48:53 +0000

    Added coverage threshold

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,5 +3,13 @@ import type {Config} from '@jest/types';
 export default async (): Promise<Config.InitialOptions> => {
   return {
     moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+    coverageThreshold: {
+      global: {
+        branches: 0,
+        functions: 0,
+        lines: 0,
+        statements: 0,
+      }
+    }
   };
 };
