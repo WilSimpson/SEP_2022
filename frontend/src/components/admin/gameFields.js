854a7abacf8100686952b66dfda67ce5206ad054
Wil Simpson <simpson.wil255@gmail.com>
2022-04-18 17:52:28 -0500

    Refactored layout and added import button

--- frontend/src/components/admin/gameFields.js
+++ frontend/src/components/admin/gameFields.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button, TextField, Grid, Switch} from '@mui/material';
 import authService from '../../services/auth';
+import {alertService} from '../../services/alert';
 
 export default function GameFields(props) {
   const isEditing = props.game != null;
@@ -22,31 +23,78 @@ export default function GameFields(props) {
 
   const editDisplay = isEditing ? {} : {display: 'none'};
 
+  const handleFileUpload = (event) => {
+    console.log('loaded file:', event.target.files[0]);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      try {
+        const content = JSON.parse(e.target.result);
+
+        if (content['title'] == null) {
+          alertService.error('No title');
+          console.log('no title');
+          return;
+        }
+
+        if (content['code'] == null) {
+          alertService.error('No code');
+          console.log('no code');
+          return;
+        }
+
+        if (content['options'] == null) {
+          alertService.error('No options');
+          console.log('no options');
+          return;
+        }
+
+        if (content['questions'] == null) {
+          alertService.error('No questions');
+          console.log('no questions');
+          return;
+        }
+
+        setTitle(content['title']);
+        setCode(content['code']);
+        setQuestonsJSON(content['questsions']);
+        setOptionsJSON(content['options']);
+        console.log('uploaded!');
+        console.log('title:', content['title']);
+      } catch (error) {
+        alertService.error('Error: File is not JSON format.');
+        console.log('error:', error);
+      }
+    };
+    reader.readAsText(event.target.files[0]);
+  };
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <h2>{isEditing ? `Edit Game: ${props.game.title}` : 'Create Game'}</h2>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} md={6} lg={3}>
         <TextField
           required
           id="outlined-required"
           label="Title"
           defaultValue={title}
           onChange={(e) => setTitle(e.target.value)}
+          sx={{width: '100%'}}
         />
       </Grid>
 
-      <Grid item xs={12} sx={editDisplay}>
+      <Grid item xs={12} md={6} lg={3} sx={editDisplay}>
         {active ? 'Active' : 'Inactive'}{' '}
         <Switch
           label="Active"
           checked={active}
           onChange={() => setActive(!active)}
+          sx={{width: '100%'}}
         />
       </Grid>
 
-      <Grid item xs={12}>
+      <Grid item xs={12} md={6} lg={3}>
         <TextField
           required
           id="outlined-required"
@@ -54,6 +102,7 @@ export default function GameFields(props) {
           defaultValue={code}
           inputProps={{maxLength: 6}}
           onChange={(e) => setCode(e.target.value)}
+          sx={{width: '100%'}}
         />
       </Grid>
 
@@ -65,6 +114,7 @@ export default function GameFields(props) {
           label="Questions JSON"
           defaultValue={questionsJSON}
           onChange={(e) => setQuestonsJSON(e.target.value)}
+          sx={{width: '100%'}}
           rows={10}
         />
       </Grid>
@@ -77,12 +127,28 @@ export default function GameFields(props) {
           label="Options JSON"
           defaultValue={optionsJSON}
           onChange={(e) => setOptionsJSON(e.target.value)}
+          sx={{width: '100%'}}
           rows={10}
         />
       </Grid>
 
+      <Grid item xs={12}>
+        <Button
+          variant='contained'
+          component='label'
+          sx={{width: '100%'}}
+        >
+          Import from File
+          <input
+            type='file'
+            onChange={handleFileUpload}
+            hidden
+          />
+        </Button>
+      </Grid>
+
       <Grid item xs={6}>
-        <Button onClick={props.onCancel}>Cancel</Button>
+        <Button variant='outlined' onClick={props.onCancel} sx={{width: '100%'}}>Cancel</Button>
       </Grid>
       <Grid item xs={6}>
         <Button
@@ -97,6 +163,7 @@ export default function GameFields(props) {
                 optionsJSON,
             );
           }}
+          sx={{width: '100%'}}
         >
           Submit
         </Button>
