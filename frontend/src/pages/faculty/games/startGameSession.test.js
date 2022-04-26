52e686a267786d6e68f167a9d059644f7c115f4f
Wil Simpson <simpson.wil255@gmail.com>
2022-04-25 20:54:56 -0500

    Fix tests

--- frontend/src/pages/faculty/games/startGameSession.test.js
+++ frontend/src/pages/faculty/games/startGameSession.test.js
@@ -39,144 +39,89 @@ afterEach(() => {
 });
 
 describe('<StartGameSessionPage />', () => {
-  let wrapper;
-
-  beforeEach(async () => {
-    wrapper = mount(
+  it ('should render Session Start component', () => {
+    const wrapper = mount(
       <BrowserRouter>
         <StartGameSessionPage />
       </BrowserRouter>
     );
+    const sessionStart = wrapper.find(SessionStart).first();
+    expect (sessionStart.exists()).toBe(true);
+  });
+  describe('SessionStart prop functions', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(
+        <BrowserRouter>
+          <StartGameSessionPage />
+        </BrowserRouter>
+      );
+    });
+    describe('onCancel functionality', () => {
+      it ('should navigate to adminDash onCancel when user is Admin', () => {
+        act(() => {wrapper.find(SessionStart).first().props().onCancel()});
+        expect(mockedNavigate).toHaveBeenCalledWith('/admin-dashboard');
+      });
+      it ('should navigate to facultyDash onCancel when user is Faculty', () => {
+        let tempUser = JSON.parse(localStorage.getItem('user'));
+        tempUser.role = 'FACULTY';
+        localStorage.setItem('user', JSON.stringify(tempUser));
+        act(() => {wrapper.find(SessionStart).first().props().onCancel()});
+        expect(mockedNavigate).toHaveBeenCalledWith('/faculty-dashboard');
+      });
+      it ('should call alert Service onCancel', () => {
+        let alertSpy = jest.spyOn(alertService, 'alert');
+        act(() => {wrapper.find(SessionStart).first().props().onCancel()});
+        expect(alertSpy).toHaveBeenCalled();
+      });
+    });
 
-    await act(async () => {
-      try {
-        await Promise.resolve(wrapper);
-        wrapper.update();
-      } catch (error) {
-        console.log('error:', error);
-      }
-    })
-  })
-
-  it ('should render', () => {
-    expect(wrapper);
-  })
-
-  it ('should render Session Start component', () => {
-    try {
-      const sessionStart = wrapper.find(SessionStart)
-    } catch (error) {
-      console.log('error:', error);
-    }
-   
-    // expect(sessionStart.exists()).toBe(true);
-  })
-
-  // describe('SessionStart prop functions', () => {
-  //   describe('onCancel functionality', () => {
-      // it ('should navigate to adminDash onCancel when user is Admin', () => {
-      //   wrapper.find(SessionStart).first().props().onCancel();
-      //   expect(mockedNavigate).toHaveBeenCalled();
-      // });
-      // it ('should navigate to facultyDash onCancel when user is Faculty', () => {
-      //   let tempUser = JSON.parse(localStorage.getItem('user'));
-      //   tempUser.role = 'FACULTY';
-      //   localStorage.setItem('user', JSON.stringify(tempUser));
-      //   act(() => {wrapper.find(SessionStart).first().props().onCancel()});
-      //   expect(mockedNavigate).toHaveBeenCalledWith('/faculty-dashboard');
-      // });
-      // it ('should call alert Service onCancel', () => {
-      //   let alertSpy = jest.spyOn(alertService, 'alert');
-      //   act(() => {wrapper.find(SessionStart).first().props().onCancel()});
-      //   expect(alertSpy).toHaveBeenCalled();
-      // });
-  //   });
-  // })
-
-  // it ('should render Session Start component', () => {
-  //   wrapper = mount(
-  //     <BrowserRouter>
-  //       <StartGameSessionPage />
-  //     </BrowserRouter>
-  //   );
-
-  //   const sessionStart = wrapper.find(SessionStart).first();
-  //   expect (sessionStart.exists()).toBe(true);
-  // });
-  // // describe('SessionStart prop functions', () => {
-  //   let wrapper;
-  //   beforeEach(() => {
-  //     wrapper = mount(
-  //       <BrowserRouter>
-  //         <StartGameSessionPage />
-  //       </BrowserRouter>
-  //     );
-  //   });
-  //   describe('onCancel functionality', () => {
-  //     it ('should navigate to adminDash onCancel when user is Admin', () => {
-  //       act(() => {wrapper.find(SessionStart).first().props().onCancel()});
-  //       expect(mockedNavigate).toHaveBeenCalledWith('/admin-dashboard');
-  //     });
-  //     it ('should navigate to facultyDash onCancel when user is Faculty', () => {
-  //       let tempUser = JSON.parse(localStorage.getItem('user'));
-  //       tempUser.role = 'FACULTY';
-  //       localStorage.setItem('user', JSON.stringify(tempUser));
-  //       act(() => {wrapper.find(SessionStart).first().props().onCancel()});
-  //       expect(mockedNavigate).toHaveBeenCalledWith('/faculty-dashboard');
-  //     });
-  //     it ('should call alert Service onCancel', () => {
-  //       let alertSpy = jest.spyOn(alertService, 'alert');
-  //       act(() => {wrapper.find(SessionStart).first().props().onCancel()});
-  //       expect(alertSpy).toHaveBeenCalled();
-  //     });
-  //   });
-
-  //   describe('onSubmit functionality', () => {
-  //     describe('on Success', () => {
-  //       it ('should call createGameSession onSubmit success', async () => {
-  //         const promise = Promise.resolve();
-  //         MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
-  //         act(() => {wrapper.find(SessionStart).props().onSubmit()});
-  //         await act(() => promise);
-  //         expect(MockGameSessionService.createGameSession).toHaveBeenCalled();
-  //       });
-  //       it ('should navigate to adminDash onSubmit success and user admin', async () => {
-  //         const promise = Promise.resolve();
-  //         MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
-  //         act(() => {wrapper.find(SessionStart).props().onSubmit()});
-  //         await act(() => promise);
-  //         expect(mockedNavigate).toHaveBeenCalledWith('/admin-dashboard');
-  //       });
-  //       it ('should navigate to facultyDash onSubmit success and user faculty', async () => {
-  //         let tempUser = JSON.parse(localStorage.getItem('user'));
-  //         tempUser.role = 'FACULTY';
-  //         localStorage.setItem('user', JSON.stringify(tempUser));
-  //         const promise = Promise.resolve();
-  //         MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
-  //         act(() => {wrapper.find(SessionStart).props().onSubmit()});
-  //         await act(() => promise);
-  //         expect(mockedNavigate).toHaveBeenCalledWith('/faculty-dashboard');
-  //       });
-  //       it ('should call alert Service', async () => {
-  //         let alertSpy = jest.spyOn(alertService, 'alert');
-  //         const promise = Promise.resolve();
-  //         MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
-  //         act(() => {wrapper.find(SessionStart).props().onSubmit()});
-  //         await act(() => promise);
-  //         expect(alertSpy).toHaveBeenCalled();
-  //       });
-  //     });
-  //     describe('on Failure', () => {
-  //       it ('should call alert Service', async () => {
-  //         let alertSpy = jest.spyOn(alertService, 'alert');
-  //         const promise = Promise.resolve();
-  //         const err = new Error('test error');
-  //         MockGameSessionService.createGameSession.mockRejectedValue(err);
-  //         act(() => {wrapper.find(SessionStart).props().onSubmit()});
-  //         await act(() => promise);
-  //         expect(alertSpy).toHaveBeenCalled();
-  //       });
-  //     });
-  //   });
-  // });
+    describe('onSubmit functionality', () => {
+      describe('on Success', () => {
+        it ('should call createGameSession onSubmit success', async () => {
+          const promise = Promise.resolve();
+          MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
+          act(() => {wrapper.find(SessionStart).props().onSubmit()});
+          await act(() => promise);
+          expect(MockGameSessionService.createGameSession).toHaveBeenCalled();
+        });
+        it ('should navigate to adminDash onSubmit success and user admin', async () => {
+          const promise = Promise.resolve();
+          MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
+          act(() => {wrapper.find(SessionStart).props().onSubmit()});
+          await act(() => promise);
+          expect(mockedNavigate).toHaveBeenCalledWith('/admin-dashboard');
+        });
+        it ('should navigate to facultyDash onSubmit success and user faculty', async () => {
+          let tempUser = JSON.parse(localStorage.getItem('user'));
+          tempUser.role = 'FACULTY';
+          localStorage.setItem('user', JSON.stringify(tempUser));
+          const promise = Promise.resolve();
+          MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
+          act(() => {wrapper.find(SessionStart).props().onSubmit()});
+          await act(() => promise);
+          expect(mockedNavigate).toHaveBeenCalledWith('/faculty-dashboard');
+        });
+        it ('should call alert Service', async () => {
+          let alertSpy = jest.spyOn(alertService, 'alert');
+          const promise = Promise.resolve();
+          MockGameSessionService.createGameSession.mockResolvedValue({data: {code: 123456}});
+          act(() => {wrapper.find(SessionStart).props().onSubmit()});
+          await act(() => promise);
+          expect(alertSpy).toHaveBeenCalled();
+        });
+      });
+      describe('on Failure', () => {
+        it ('should call alert Service', async () => {
+          let alertSpy = jest.spyOn(alertService, 'alert');
+          const promise = Promise.resolve();
+          const err = new Error('test error');
+          MockGameSessionService.createGameSession.mockRejectedValue(err);
+          act(() => {wrapper.find(SessionStart).props().onSubmit()});
+          await act(() => promise);
+          expect(alertSpy).toHaveBeenCalled();
+        });
+      });
+    });
+  });
 });
