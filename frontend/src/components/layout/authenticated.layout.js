d83dab6fdac3797b597254124434ab10c200d85f
Ella Norton <ella-norton@uiowa.edu>
2022-04-26 20:06:13 -0500

    add app bar to dash

--- frontend/src/components/layout/authenticated.layout.js
+++ frontend/src/components/layout/authenticated.layout.js
@@ -2,33 +2,35 @@ import React from 'react';
 import {Box, Grid, Toolbar} from '@mui/material';
 import {PageAlert} from './alert';
 import {SideMenu} from './sideMenu';
-
+import DefaultLayout from './default.layout';
 export default function AuthenticatedLayout(props) {
   return (
     <>
-      <SideMenu>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light' ?
-                theme.palette.grey[100] :
-                theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Grid container justifyContent="center" spacing={2}>
-            <Grid item xs={6}>
-              <PageAlert />
+      <DefaultLayout>
+        <SideMenu>
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light' ?
+                  theme.palette.grey[100] :
+                  theme.palette.grey[900],
+              flexGrow: 1,
+              height: '100vh',
+              overflow: 'auto',
+            }}
+          >
+            <Toolbar />
+            <Grid container justifyContent="center" spacing={2}>
+              <Grid item xs={6}>
+                <PageAlert />
+              </Grid>
             </Grid>
-          </Grid>
-          <Toolbar />
-          {props.children}
-        </Box>
-      </SideMenu>
+            <Toolbar />
+            {props.children}
+          </Box>
+        </SideMenu>
+      </DefaultLayout>
     </>
   );
 }
